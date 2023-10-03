fee7db9aeab58dd38b10822625010e2833ae81a1
Zijia Liu <80761505+abinicorn@users.noreply.github.com>
2023-10-03 16:12:41 +1300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -22,7 +22,7 @@ const app = express();
 const port = process.env.PORT || 3001;
 
 app.use((req, res, next) => {
-    // res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+    // res.header('Access-Control-Allow-Origin', 'http://localhost:3000'); hello world
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     res.setHeader('Access-Control-Allow-Origin', '*');
